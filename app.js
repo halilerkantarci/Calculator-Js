fb0ce116e637f46cbca688f1da32ff3557c39f51
halil <halilerk77@icloud.com>
2022-06-08 14:12:19 +0300

    completed

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const ekran = document.querySelector(".ekran");
 const tuslar = document.querySelector(".tuslar");
 
 let ekranValue = "0";
-let first = null;
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
 
 tuslar.addEventListener("click", (e) => {
   const element = e.target;
 
   if (!element.matches("button")) return;
   if (element.classList.contains("operator")) {
+    handleOperator(element.value);
+    updateEkran();
     return;
   }
 
@@ -40,9 +44,48 @@ function clear() {
 }
 
 function inputNumber(num) {
-  if (ekranValue === "0") {
-    return (ekranValue = num);
+  if (waitingForSecondValue) {
+    ekranValue = num;
+    waitingForSecondValue = false;
   } else {
-    return (ekranValue = ekranValue + num);
+    ekranValue = ekranValue === "0" ? num : ekranValue + num;
   }
+
+  console.log(ekranValue, firstValue, operator, waitingForSecondValue);
+}
+
+function handleOperator(nextOperator) {
+  const value = parseFloat(ekranValue);
+
+  if (operator && waitingForSecondValue) {
+    operator = nextOperator;
+    return;
+  }
+
+  if (firstValue === null) {
+    firstValue = value;
+  } else if (operator) {
+    const result = calculate(firstValue, value, operator);
+
+    ekranValue = `${parseFloat(result.toFixed(7))}`;
+    firstValue = result;
+  }
+
+  waitingForSecondValue = true;
+  operator = nextOperator;
+
+  console.log(ekranValue, firstValue, operator, waitingForSecondValue);
+}
+
+function calculate(first, second, operator) {
+  if (operator === "+") {
+    return first + second;
+  } else if (operator === "-") {
+    return first - second;
+  } else if (operator === "*") {
+    return first * second;
+  } else if (operator === "/") {
+    return first / second;
+  }
+  return second;
 }
